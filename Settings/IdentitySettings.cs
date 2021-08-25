370e83fc280a74898ceff3b355d13a10c7d90099
Ravi <ravi@Ravis-MacBook-Air.local>
2021-08-25 23:20:39 +0530

    made it all working

--- Settings/IdentitySettings.cs
+++ Settings/IdentitySettings.cs
@@ -2,8 +2,8 @@ namespace Play.Identity.Service.Settings
 {
     public class IdentitySettings
     {
-        public string AdminUserEmail { get; init; }
-        public string AdminUserPassword { get; init; }
+        public string AdminUserEmail { get; init; } = "ravi1408@gmail.com";
+        public string AdminUserPassword { get; init; } = "Asdzxc@987";
         public decimal StartingGil { get; init; }
     }
 }
